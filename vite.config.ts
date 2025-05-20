b4bf873cbdf126734cbd3d7c3136fd41650528d8
Erik Beltran Morales <erikb0696@gmail.com>
2025-05-19 20:41:58 -0500

    Add alias for @components

--- vite.config.ts
+++ vite.config.ts
@@ -3,16 +3,17 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import tailwindcss from '@tailwindcss/vite'
+
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  plugins: [vue(), vueDevTools(), tailwindcss()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '@components': path.resolve(__dirname, './src/components'),
     },
   },
 })
