2fea888d219845e26a5424e26ff2ce30fb20b7a2
Erik Beltran Morales <erikb0696@gmail.com>
2025-05-20 12:47:37 -0500

    Render new releases in home

---
+++ src/components/SongCard.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import type { Song } from '@/types/song'
+import CardPlayButton from './CardPlayButton.vue'
+
+interface Props {
+  song: Song
+}
+defineProps<Props>()
+</script>
+
+<template>
+  <article
+    class="group relative hover:bg-zinc-800 shadow-lg hover:shadow-xl rounded-md ransi transition-all duration-300"
+  >
+    <div
+      class="absolute right-4 bottom-30 transition-all duration-500 group-hover:translate-y-0 group-hover:opacity-100 z-10 translate-y-4 opacity-0"
+    >
+      <CardPlayButton class="text-4xl" />
+    </div>
+
+    <RouterLink
+      to="/"
+      class="playlist-item transition-all duration-300 flex relative p-2 overflow-hidden gap-2 pb-6 rounded-md w-44 flex-col"
+    >
+      <picture class="h-auto w-full flex-none aspect-auto">
+        <img :src="song.images[0]?.url" alt="song cover" class="rounded mb-2" />
+      </picture>
+      <div class="flex flex-auto flex-col px-2">
+        <h4 v-text="song.name" class="font-medium text-white text-sm" />
+        <p class="text-gray-400 text-xs">
+          {{ song.artists.map((artist) => artist.name).join(', ') }}
+        </p>
+      </div>
+    </RouterLink>
+  </article>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,15 @@
 <script setup lang="ts">
+import SongCard from '@/components/SongCard.vue'
 import { getNewReleases } from '@/services/spotifyApi'
+import type { Song } from '@/types/song'
 import { onMounted, ref } from 'vue'
 
-const newReleases = ref([])
+const newReleases = ref<Song[]>([])
 
 onMounted(async () => {
   try {
     newReleases.value = await getNewReleases()
-    console.log(newReleases.value) // Imprimir el array de nuevos lanzamientos
+    console.log(newReleases.value)
   } catch (err) {
     console.error('Failed to load new releases', err)
   }
@@ -15,7 +17,17 @@ onMounted(async () => {
 </script>
 
 <template>
-  <main>
-    <!-- <TheWelcome /> -->
+  <main class="[grid-area:main] rounded-lg bg-zinc-900 overflow-y-auto w-full p-6">
+    <div>
+      <h2 class="text-2xl font-bold">New Releases</h2>
+      <div class="flex overflow-x-auto gap-x-4 pb-2">
+        <SongCard
+          v-for="song in newReleases"
+          :key="song.id"
+          :song="song"
+          class="min-w-[150px] flex-shrink-0"
+        />
+      </div>
+    </div>
   </main>
 </template>
