24ebb48d757c1effd6d9ead08a2a88378bb06981
Erik Beltran Morales <erikb0696@gmail.com>
2025-06-09 13:29:47 -0500

    reload artist data in ArtistView when route param id changes

--- src/views/ArtistView.vue
+++ src/views/ArtistView.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { onMounted, ref } from 'vue'
+import { onMounted, ref, watch } from 'vue'
 import { useRoute } from 'vue-router'
 
 import ArtistContainer from '@/components/ArtistContainer.vue'
@@ -12,18 +12,30 @@ const route = useRoute()
 const artist = ref<Artist>()
 const topTracks = ref<TopTrack[]>([])
 
-const {
-  params: { id },
-} = route
-
-onMounted(async () => {
+async function loadArtistData(id: string) {
   try {
     artist.value = await getArtist(id)
     topTracks.value = await getArtistTopTracks(id)
   } catch (err) {
-    console.error('Failed to load new releases', err)
+    console.error('Failed to load artist data', err)
+  }
+}
+
+onMounted(() => {
+  if (route.params.id) {
+    loadArtistData(route.params.id as string)
   }
 })
+
+// Observa cambios en el id y recarga los datos
+watch(
+  () => route.params.id,
+  (newId, oldId) => {
+    if (newId && newId !== oldId) {
+      loadArtistData(newId as string)
+    }
+  },
+)
 </script>
 
 <template>
